77488dc608f1a1b9f3dd2654ff690cd46a034ef3
ArionDutta33 <ariondutta@333>
2024-11-21 19:11:36 +0530

    Bottom sheet and handle submit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { Ionicons } from '@expo/vector-icons';
 import BottomSheet, {
   BottomSheetFlatList,
   BottomSheetModal,
@@ -6,13 +7,15 @@ import BottomSheet, {
 } from '@gorhom/bottom-sheet';
 import moment from 'moment';
 import React, { useCallback, useRef, useMemo, useState } from 'react';
-import { View, Text, FlatList, Button, StyleSheet } from 'react-native';
+import { View, Text, FlatList, Button, StyleSheet, TextInput, ScrollView } from 'react-native';
 import { FloatingAction } from 'react-native-floating-action';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { tasks } from '~/assets/data/tasks';
 import TaskItemBox from '~/components/customComponents/taskBox';
 const Home = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
   const bottomSheetModalRef = useRef<BottomSheetModal>(null);
 
   // callbacks
@@ -52,6 +55,8 @@ const Home = () => {
         <BottomSheetModalProvider>
           {/* <Button onPress={handlePresentModalPress} title="Present Modal" color="black" /> */}
           <BottomSheetModal
+            enablePanDownToClose={false}
+            enableHandlePanningGesture={false} // Disable handle dragging
             backgroundStyle={{ backgroundColor: 'rgb(39,38,38)' }}
             animateOnMount
             animationConfigs={{
@@ -61,11 +66,36 @@ const Home = () => {
               restSpeedThreshold: 0.1,
             }}
             onDismiss={() => {}}
-            snapPoints={['50%']}
+            snapPoints={['30%']}
             ref={bottomSheetModalRef}
             onChange={handleSheetChanges}>
-            <BottomSheetView>
-              <Text>Awesome 🎉</Text>
+            <BottomSheetView className="">
+              <TextInput
+                value={title}
+                onChangeText={setTitle}
+                className="px-6   text-2xl tracking-widest  text-white"
+                placeholderTextColor="#ddd"
+                placeholder="Task name"
+              />
+              <TextInput
+                value={description}
+                onChangeText={setDescription}
+                multiline={false}
+                className="border-white  px-6 text-lg tracking-widest  text-white"
+                placeholderTextColor="#ddd"
+                placeholder="Description"
+              />
+              <View className=" ">
+                <Ionicons
+                  onPress={() => {
+                    console.log('Email:', title, '\n description', description);
+                  }}
+                  className={` mx-6 my-4  self-end rounded-xl border bg-[crimson] p-3 px-5`}
+                  name="send"
+                  size={24}
+                  color="white"
+                />
+              </View>
             </BottomSheetView>
           </BottomSheetModal>
         </BottomSheetModalProvider>
