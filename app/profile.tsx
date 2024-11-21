3164937bfbb6ec77252429b7a093aa84c1fb67a0
ArionDutta33 <ariondutta@333>
2024-11-21 23:55:24 +0530

    Charts added

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,26 +1,48 @@
 import { Redirect } from 'expo-router';
 import React from 'react';
 import { View, Text, Pressable } from 'react-native';
+import { BarChart, LineChart, PieChart, PopulationPyramid } from 'react-native-gifted-charts';
 
 import { useAuth } from '~/provider/AuthProvider';
 import { supabase } from '~/utils/supabase';
 
 const Profile = () => {
   const { isAuthenticated } = useAuth();
+  const pieData = [
+    { value: 70, color: 'crimson' },
+
+    { value: 30, color: 'gray' },
+  ];
+
   if (!isAuthenticated) {
     return <Redirect href="/(auth)/login" />;
   }
   return (
-    <View>
-      <Text>Profile</Text>
-      <Pressable
-        onPress={() => {
-          console.log('logout');
-          supabase.auth.signOut();
-        }}>
-        <Text>Logout</Text>
-      </Pressable>
-    </View>
+    <>
+      <View className="flex-1  bg-[rgb(39,38,38)]">
+        <Text>Profile</Text>
+        <Pressable
+          onPress={() => {
+            console.log('logout');
+            supabase.auth.signOut();
+          }}>
+          <Text>Logout</Text>
+        </Pressable>
+        <View>
+          <PieChart
+            donut
+            innerRadius={110}
+            animationDuration={1000}
+            isAnimated
+            innerCircleColor="rgb(39,38,38)]"
+            data={pieData}
+            centerLabelComponent={() => {
+              return <Text style={{ fontSize: 30, color: 'white' }}>70%</Text>;
+            }}
+          />
+        </View>
+      </View>
+    </>
   );
 };
 
